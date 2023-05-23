b8e38db6e1c786a01a79e2d12e2c5759cb187479
SinX <punisherdu28@gmail.com>
2023-05-23 12:07:33 +0200

    Ajout 2 classes Classe & Etudiant dans 2 fchiers éponymes

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
-class Bird:
+from etudiant import Etudiant
+from classe import Classe 
 
-    def __init__(self,name,size):
-        self.name=name
-        self.size=size
+def initialisation():
+    
+    e1 = Etudiant("Jhon","Doe",15)
+    
+    grp = [
+        e1,
+        Etudiant("Walid","Taleb",35),
+        Etudiant("Therence","Boisgard",23),
+        Etudiant("Velan","Segutuvan",24),
+        Etudiant("Raphael","Marouani",23)
+    ]
 
+    e2=Etudiant("Jean","Claude",35)
+    e3=Etudiant("Van","Damn",35)
+    e4=Etudiant("Victor","Chuun",35)
+    e5=Etudiant("Kevin","Leroux",35)
 
-    def fly(self):
-        print(f"Un {self.name} est en train de voler")
+    print("Création des classes")
+    
+    c2 = Classe("M2 IRS")
+    c1 = Classe("M1 IRS",grp)
+    
+
+    print("Incrémentation d'élèves dans la classe")
+
+    c2.addStudent(e2)
+    c2.addStudent(e3)
+    c2.addStudent(e4)
+    c2.addStudent(e5)
+
+    print("Vue sur les classes")
+    
+    c1.view_class()
+    c2.view_class()
+
+    print("Moyenne d'age des classes de cours:")
+    print(f"M1-IRS:{c1.age_moyen()}\nM2-IRS:{c1.age_moyen()}")   
 
 
 if __name__=="__main__":    
-    b1= Bird("Hirondelle",16)
-    b1.fly()
+    initialisation()
